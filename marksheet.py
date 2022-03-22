33ae6b47bc089fdd04f9fdd856cb0b6e9ace9c84
Abhijai Singh <abhijaisingh15@gmail.com>
2022-03-22 08:13:08 +0530

    Update marksheet.py

--- marksheet.py
+++ marksheet.py
@@ -1,5 +1,8 @@
 import csv
 with open('student_marks.csv', 'r') as f:
     content = csv.reader(f, delimiter=',')
-    for i in content:
-        print(content[2][1])
+    for line_number, line in enumerate(content):
+        if line_number == 0:
+            continue
+        else:
+            print(line)
